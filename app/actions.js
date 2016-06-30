14ac14c4c8d9122b296e450f75ba4be6f4f3dc2b
Claire Liu <wenjing.liu09@foxmail.com>
2016-06-30 17:24:55 +0800

    reducers

--- app/actions.js
+++ app/actions.js
@@ -7,3 +7,25 @@ export const VisibilityFilters = {
   SHOW_COMPLETED: 'SHOW_COMPLETED',
   SHOW_ACTIVE: 'SHOW_ACTIVE'
 };
+
+
+export function addTodo(text){
+  return {
+    type: ADD_TODO,
+    text
+  }
+}
+
+export function completeTodo(index){
+  return {
+    type: COMPLETE_TODO,
+    index
+  }
+}
+
+export function setVisibilityFilter(filter) {
+  return {
+    type: SET_VISIBILITY_FILTER,
+    filter
+  }
+}
